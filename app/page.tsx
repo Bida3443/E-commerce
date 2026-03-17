dcd801de2ab77926ee1dc95ac8c28f03fedc453d
Bida3443 <alexanderbamise@gmail.com>
2026-03-17 04:51:08 -0700

    Ecommerce

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,48 @@
-import Image from "next/image";
+// import Image from "next/image";
+
+import { Key } from "lucide-react";
+import { text } from "stream/consumers";
+
+const Nav = [
+  {
+    text: "Home",
+    Link: "/",
+  },
+  {
+    text:"About",
+    link:"/",
+  },
+  {
+    text:"Contact",
+    link:"/"
+  },
+  {
+    text:"Services",
+    link:"/",
+  },
+  {
+    text:"Blog",
+    link:"/"
+  },
+]
 
 export default function Home() {
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main>
-        <div>hello world</div>
+        <div>
+          {Nav.map((Nav,index)=>(
+            <div key={index}>
+              <div>{Nav.text}</div>
+              <div>{Nav.link}</div>
+            </div>
+            
+          ))}
+        </div>
       </main>
     </div>
   );
 }
 
-s
+

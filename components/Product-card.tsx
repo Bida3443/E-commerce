6f6c3bf7f052ba2b9033917df668441d8efedd6b
Bida3443 <alexanderbamise@gmail.com>
2026-03-22 05:54:40 -0700

    Ecommerce

---
+++ components/Product-card.tsx
@@ -0,0 +1,47 @@
+import Stripe from "stripe";
+import Link from "next/link";
+// import { link } from "fs";
+import { Card } from "./ui/card";
+import Image from "next/image";
+
+
+interface props {
+    products: Stripe.Product[];
+}
+
+export const ProductCard = ({ products }: props) => {
+
+    return (
+        <div> 
+            <div>
+                <input type="text" placeholder="Search products...." />
+            </div>
+            <ul>
+                {products.map((product)=>{
+                    return (
+                        <Link href={"/product/1"}>
+                            <Card>
+                                 {product.images && product.images[0] && (
+                                            <div className="relative flex justify-center items-center h-80 w-full">
+                                                <Image
+                                
+                                                alt={product.name}
+                                                src={product.images[0]}
+                                                width={380}
+                                                height={100}
+                                                objectFit="cover"
+                                                className="transition-opacity duration-500 ease-in-out"/>
+                                            </div>
+                                
+                                        )}
+
+                            </Card>
+                        </Link>
+                    )
+                })}
+            </ul>
+        </div>
+
+    );
+
+};
